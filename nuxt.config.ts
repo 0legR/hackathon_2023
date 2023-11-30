f2a162e12b3b1220bd740e3bcdbed17e90bb2270
олег ростов <terabyte.or@gmail.com>
2023-11-30 16:57:04 +0100

    first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
+  devServer: { port: 5001 },
   vue: {
     compilerOptions: {
       isCustomElement: (tag) => ['UseFetchDemo'].includes(tag),
