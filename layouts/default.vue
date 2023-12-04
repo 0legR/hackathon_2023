7b1222cee0398efea172d756e4c087915abb8584
олег ростов <terabyte.or@gmail.com>
2023-12-04 15:58:50 +0100

    lading track

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,15 @@
 .bg-dark.w-full.bg-img
   slot
 </template>
+<script setup>
+useHead({
+  title: 'Landing',
+  link: {
+    rel: 'icon',
+    href: '/logo.png',
+  },
+})
+</script>
 <style>
 .bg-img {
   /* background-image: url('matrix.gif'); */
